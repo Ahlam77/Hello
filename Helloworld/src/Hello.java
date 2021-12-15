2c6c99f954030662b240e965f83b404337455631
ahlam77 <ama1977ln@outlook.com>
2021-12-15 12:58:29 +0100

    creating java project

---
+++ Helloworld/src/Hello.java
@@ -0,0 +1,15 @@
+
+ public class Hello {
+
+	public static void main(String[] args) {
+
+     String Fname;
+      Fname = " Ahlam ";
+      System.out.println(Fname);
+
+       java.util.Date dt = new java.util.Date();
+        System.out.println(dt);
+
+	}
+
+}
